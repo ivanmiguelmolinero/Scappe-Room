c8730829a15dc219bd3118cdd703612ad5c2c6a6
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2021-07-09 13:45:31 +0200

    Fase final terminada

--- Proyecto/JS/final.js
+++ Proyecto/JS/final.js
@@ -1,8 +1,26 @@
 function main() {
 
-    $("#botonQueSeMueve").hover(function () {
-        $("#botonQueSeMueve").css("top", `${Math.random() * 500}px`);
-        $("#botonQueSeMueve").css("left", `${Math.random() * 800}px`);
-      });
+    //ABACHITO!!!
+    let si = document.getElementById('si');
+    let abachito = document.getElementById('abachito');
+    //Poner música de fondo en Google Chrome
+    let play = document.getElementById('play');
+    let musica = document.getElementById('musica');
+    //Risa cuando el NO se mueve
+    let no = document.getElementById('botonQueSeMueve');
+    let risa = document.getElementById('risa');
+
+    si.onclick = () => {
+      abachito.play();
+      window.alert('¡¡¡¡ABACHITO!!!!');
+    }
+
+    play.onclick = () => {
+      musica.play();
+    }
+
+    no.onmouseover = () => {
+      risa.play();
+    }
 
 }
